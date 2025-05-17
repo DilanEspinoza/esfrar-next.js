c15b453f0c94bc31d2151035d4d0174d4a25560e
Dilan Espinoza <dilan.developer22@gmail.com>
2025-05-17 12:16:56 -0500

    add splash screen

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
-import { UserProvider } from "@/context/UserContext";
+"use client"; // Esto indica que este archivo es un componente de cliente
+
+import { useEffect, useState } from "react";
 import { Poppins } from "next/font/google";
+import SplashScreen from "../components/SplashScreen/SplashScreen"; // Asegúrate de tener el componente
 import "./globals.css";
-import ClientWrapper from "@/components/ClientWrapper";
+import { UserProvider } from "@/context/UserContext"; // Si tienes contexto de usuario
+import { metadata } from "./metadata"; // Aquí importas el metadata
 
 const poppins = Poppins({
 	subsets: ["latin"],
@@ -9,16 +13,45 @@ const poppins = Poppins({
 	variable: "--font-poppins",
 });
 
-export const metadata = {
-	title: "Esfrar",
-	description: "Una galeria de imagenes",
-};
-
 export default function RootLayout({ children }) {
+	const [showSplash, setShowSplash] = useState(false);
+	const [fadeOut, setFadeOut] = useState(false);
+	const [appReady, setAppReady] = useState(false);
+
+	useEffect(() => {
+		const visited = localStorage.getItem("visited");
+
+		if (!visited) {
+			localStorage.setItem("visited", "true");
+			setShowSplash(true);
+
+			const splashTimer = setTimeout(() => {
+				setFadeOut(true); // Inicia el fade-out
+				setTimeout(() => {
+					setShowSplash(false);
+					setAppReady(true); // Carga la app después del fade-out
+				}, 700); // Tiempo de fade-out
+			}, 2500); // Tiempo que el splash estará visible
+
+			return () => clearTimeout(splashTimer); // Limpiar el timer cuando el componente se desmonte
+		} else {
+			setAppReady(true); // Si ya visitó la app, la carga inmediatamente
+		}
+	}, []);
+
 	return (
 		<html lang='es'>
+			<head>
+				<meta name='description' content={metadata.description} />
+				<title>{metadata.title}</title>
+				{/* Aquí puedes agregar más meta tags u otros elementos necesarios */}
+			</head>
 			<body className={`${poppins.variable} antialiased`}>
-				<ClientWrapper>{children}</ClientWrapper>
+				<UserProvider>
+					{showSplash && <SplashScreen isVisible={!fadeOut} />}
+					{appReady && children}{" "}
+					{/* Carga el contenido de la app solo después de la animación */}
+				</UserProvider>
 			</body>
 		</html>
 	);
---
+++ src/app/metadata.js
@@ -0,0 +1,4 @@
+export const metadata = {
+	title: "Esfrar",
+	description: "Una galería de imágenes",
+};
