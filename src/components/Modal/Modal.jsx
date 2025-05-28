920bbcbb6ca5ba726646edf386c84debbb463d6c
Dilan Espinoza <dilan.developer22@gmail.com>
2025-05-28 01:01:11 -0500

    feat: add modal to dashboard

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,7 +8,7 @@ export const Modal = ({ isOpen, onClose, children }) => {
 
     return (
         <div
-            className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+            className="fixed inset-0 bg-[#000000b9] bg-opacity-40 backdrop-blur-sm flex items-center justify-center z-50"
             onClick={onClose}
         >
             <div
@@ -18,6 +18,7 @@ export const Modal = ({ isOpen, onClose, children }) => {
                 <button
                     className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
                     onClick={onClose}
+                    aria-label="Cerrar modal"
                 >
                     ✕
                 </button>
