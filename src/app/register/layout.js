0f0590f26ba35f06966c42f9a654f98198e894ad
Dilan Espinoza <dilan.developer22@gmail.com>
2025-06-07 01:29:54 -0500

    perf: change name of layou and add redux

--- src/app/register/layout.js
+++ src/app/register/layout.js
@@ -3,10 +3,6 @@ export const metadata = {
 	description: "Crear una nueva cuenta en Esfrar",
 };
 
-export default function RootLayout({ children }) {
-	return (
-		<html lang='es'>
-			<body>{children}</body>
-		</html>
-	);
+export default function RegiserLayout({ children }) {
+	return <>{children}</>;
 }
